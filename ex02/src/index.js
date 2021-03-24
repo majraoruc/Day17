aa6713b7b5e8f26d93174534f54200031edd15a3
Majra <majra.oruc@gmail.com>
2021-03-24 23:44:07 +0100

    Day 17 complete

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+const MY_CAR = {
+    wheels: 4,
+    cylinders: 4,
+    size: 2.2
+};
+
+function main(myCar) {
+    // Only change code below this line
+    return { cylinders, size } = { cylinders: myCar.cylinders, size: myCar.size };
+    //Only change code above this line
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
