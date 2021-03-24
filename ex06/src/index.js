eeb2cfbe30c1eac77e265f532a80be21c0eada3e
Majra <majra.oruc@gmail.com>
2021-03-24 23:48:06 +0100

    EX06 FIX

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -2,7 +2,7 @@ const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
     "use strict";
     //Only change code below this line
-    const [a, b, ...array2] = list;
+    const [, , ...array2] = list;
     //Only change code above this line
     return array2;
 }
